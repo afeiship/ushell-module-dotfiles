00e9d8553168ce4af5e6ec97749b6637a3622774
feizheng <1290657123@qq.com>
2016-10-24 22:48:16 +0800

    update un/in

--- index.sh
+++ index.sh
@@ -3,5 +3,7 @@
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
 alias dot-backup='cd ~/.dotfiles && git add --all && git commit -m "AUTO BACKUP~" && git push && cd -';
+alias dot-in='cd ~/.dotfiles && ./bin/dfm install';
+alias dot-un='cd ~/.dotfiles && ./bin/dfm uninstall';
 
 unset ROOT_PATH;
