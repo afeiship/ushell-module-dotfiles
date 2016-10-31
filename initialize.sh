be2304603dfcb8ea13c7ee5e1eda6d4af757e0d2
feizheng <1290657123@qq.com>
2016-10-31 14:52:03 +0800

    update initialize

--- initialize.sh
+++ initialize.sh
@@ -4,4 +4,15 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 
 
 
+## for dotfiles:
+yum install gcc perl-devel;
+cd ~/soft
+wget http://search.cpan.org/CPAN/authors/id/I/IL/ILYAM/Data-Dumper-2.121.tar.gz;
+tar xvzf Data-Dumper-2.121.tar.gz;
+cd Data-Dumper-2.121;
+perl Makefile.PL;
+make && make install;
+
+
+
 unset ROOT_PATH;
